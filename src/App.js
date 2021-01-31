54f31bf40a598583b759ef84435863ca2a4b4592
aminos02 <azzouzamine02@gmail.com>
2021-01-31 14:31:22 +0100

    SideBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app_body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
+import "./Header.css";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import { HelpOutline } from "@material-ui/icons";
 function Header() {
   return (
     <div className="header">
-      <div className="header_lef">
+      <div className="header_left">
         <Avatar className="header_avatar" alt="avatar" />
         <AccessTimeIcon />
       </div>
-      <div className="header_search"></div>
-      <div className="header_right"></div>
+      <div className="header_search">
+        <SearchIcon />
+        <input placeholder="Search " type="text" />
+      </div>
+      <div className="header_right">
+        <HelpOutline />
+      </div>
     </div>
   );
 }
